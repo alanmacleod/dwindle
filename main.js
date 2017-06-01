b43ae2da1fb6bc9e3c4347e5c11c6aabc5c8dfc7
Alan MacLeod <amacleod@rskorbital.com>
2017-06-01 12:08:30 +0700

    calc effective area

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
+//Julius / selina, oslo astra
+
 var fs = require('fs');
 
 var de = JSON.parse(fs.readFileSync("./data/de.4839.geo.json"));
 
-console.log(de.features.length);
+console.log(de.features[0].geometry.coordinates);
