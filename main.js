2f69d7fb20598454b525cbba8b59e2b88ae248d1
Alan MacLeod <amacleod@rskorbital.com>
2017-06-15 20:15:58 +0700

    nanoq dep

--- main.js
+++ main.js
@@ -1,8 +1,41 @@
 
+"#ifndef PI"
+  "#define PI 3.141592653589793"
+"#endif"
+
+import nanoq from 'nanoq';
+
+let nq = new nanoq(null, function(a,b){
+  return a > b;
+});
+
+for (var t=0; t<100; t++)
+{
+  nq.push((Math.random()*100)>>0);
+}
+
+
+"#ifdef PI"
+  console.log(nq.tree);
+"#endif"
+
+
+for (var t=0; t<nq.length(); t++)
+{
+  console.log(nq.pop());
+}
+
+// for (var t=0; t<1000; t++)
+// {
+//   nq.push((Math.random()*1000)>>0);
+// }
+// console.log(nq.tree);
+//
+// console.log("Pop...");
+//
+// for (var t=0; t<1000; t++)
+// {
+//   console.log(nq.pop());
+// }
+
 //Julius / selina, oslo astra
-
-var fs = require('fs');
-
-var de = JSON.parse(fs.readFileSync("./data/de.4839.geo.json"));
-
-console.log(de.features[0].geometry.coordinates);
